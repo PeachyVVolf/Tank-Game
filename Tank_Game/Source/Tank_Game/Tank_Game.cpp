bae855867ef75d8a64a7b82f79337d34e21fd4b7
PeachyVVolf <ahmadajmal9301@gmail.com>
2020-08-16 23:05:42 +0500

    Added Terrain to my Tank Game

---
+++ Tank_Game/Source/Tank_Game/Tank_Game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tank_Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tank_Game, "Tank_Game" );
---
+++ Tank_Game/Source/Tank_Game/Tank_GameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Tank_GameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANK_GAME_API ATank_GameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
